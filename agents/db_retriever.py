d765caeeef58e98cde977650346b86df04f089b9
Navya <navyac.social@gmail.com>
2024-12-19 19:51:57 +0530

    db_agent created

---
+++ agents/db_retriever.py
@@ -0,0 +1,37 @@
+from crewai import Agent,Task,Crew,LLM
+from crewai_tools import SerperDevTool
+from litellm import completion
+from langchain.tools import tool
+from langchain_community.vectorstores import Chroma
+import streamlit as st
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+llm = LLM(
+    api_key=st.secrets['GOOGLE_API'],
+    model="gemini/gemini-1.5-flash",
+)
+embedder = GoogleGenerativeAIEmbeddings(model="models/embedding-001",
+                                          google_api_key=st.secrets['GOOGLE_API'])
+class getinfo:
+    @tool("Get Info Tool")
+    def info(query):
+        """Search Chroma DB for relevant news information based on a query."""
+        vectorstore = Chroma(persist_directory="./Database", embedding_function=embedder)
+        retriever = vectorstore.similarity_search(query)
+        return retriever
+    
+db_agent = Agent(
+    role='Retriever',
+    goal='Retrieve information from the database based on the user query.',
+    backstory='Expert in querying databases for precise and relevant information only if the query is related to the content in the document.',
+    tools=[getinfo().info],
+    allow_delegation=True,
+    verbose=True,
+    llm=llm
+)
+
+db_task = Task(
+    description='Retrieve information from the database based on the user query if the query is related to the content in the document.',
+    agent=db_agent,
+    expected_output='A precise answer from the database for the query.'
+)
